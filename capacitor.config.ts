1c132c3c660238bf16d3cf91e4ae5b99ba8a11a9
milodevs <siliconvalleyn91@gmail.com>
2024-09-26 10:00:10 +0300

    added capactior files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.milodevs.evride',
+  appName: 'evride',
+  webDir: '.svelte-kit/output'
+};
+
+export default config;
